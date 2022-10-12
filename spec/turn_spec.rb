7b99bbd0a72b345a946c70e8325290204a2b74f9
Meredith Trevino <meredithtrevino@gmail.com>
2022-10-11 19:02:47 -0600

    add working card lib and spec

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,4 +1,5 @@
 require './lib/turn'
+require './lib/card'
 
 RSpec.describe Turn do
   it 'exists' do
@@ -6,22 +7,3 @@ RSpec.describe Turn do
 
     expect(card).to be_instance_of(Turn)
   end
-
-#   it 'has a question' do
-#     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-#
-#     expect(card.question).to eq("What is the capital of Alaska?")
-#   end
-#
-#   it 'has an answer' do
-#     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-#
-#     expect(card.answer).to eq("Juneau")
-#   end
-#
-#   it 'has a category' do
-#     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-#
-#     expect(card.category).to eq(:Geography)
-#   end
-# end
