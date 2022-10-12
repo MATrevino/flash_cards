ef9ab656a1dbecb93f8f26da7898ae326a08e480
Meredith Trevino <meredithtrevino@gmail.com>
2022-10-12 17:59:33 -0600

    completed iteration 1

--- lib/card.rb
+++ lib/card.rb
@@ -2,10 +2,21 @@ class Card
   attr_reader :question,
                 :answer,
                 :category
-
-def initialize(question, answer, category)
+ def initialize(question, answer, category)
   @question = question
   @answer = answer
   @category = category
+ end
 end
+
+def question
+  @question
+end
+
+def answer
+  @answer
+end
+
+def category
+  @category
 end
