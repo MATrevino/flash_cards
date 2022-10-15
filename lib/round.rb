b3abef63b4e14a991e9ea1242783c0e035aac66e
Meredith Trevino <meredithtrevino@gmail.com>
2022-10-14 18:02:44 -0600

    adds working turns method

--- lib/round.rb
+++ lib/round.rb
@@ -1,9 +1,24 @@
 class Round
+  attr_reader :deck, :turns
 
-def initialize
+  def initialize(deck)
+    @deck = deck
+    @turns = []
+  end
 
-end
-end
+
+
+# def current_card
+#   deck.cards.first
+# end
+
+#turns, current_card, take_turn, class, correct?, number_correct, current_card, count,
+
 
 
 #create take_turn method
+# def take_turn(guess)
+# turn += 1
+# end
+
+end
